fb3bb504ee5b94be88f83b38215898cb0be7e367
Jobson Ribeiro <jobsonrisoa@gmail.com>
2026-05-11 11:41:11 -0300

    refactor(platform): reduce service configuration duplication

--- services/notification-service/src/test/java/com/jobson/market/notification_service/TestcontainersConfiguration.java
+++ services/notification-service/src/test/java/com/jobson/market/notification_service/TestcontainersConfiguration.java
@@ -14,18 +14,18 @@ class TestcontainersConfiguration {
   @Bean
   @ServiceConnection
   KafkaContainer kafkaContainer() {
-    return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+    return new KafkaContainer(DockerImageName.parse("apache/kafka-native:3.8.0"));
   }
 
   @Bean
   @ServiceConnection
   PostgreSQLContainer postgresContainer() {
-    return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    return new PostgreSQLContainer(DockerImageName.parse("postgres:16"));
   }
 
   @Bean
   @ServiceConnection(name = "redis")
   GenericContainer<?> redisContainer() {
-    return new GenericContainer<>(DockerImageName.parse("redis:latest")).withExposedPorts(6379);
+    return new GenericContainer<>(DockerImageName.parse("redis:7")).withExposedPorts(6379);
   }
 }
